a55129a98245d68d098af97266c89019c2d9035d
Albrecht Schmidt <Albrecht.Schmidt@t-systems.com>
2017-11-23 00:03:57 +0100

    geometry and state nav

--- src/pentapxlz/specter.clj
+++ src/pentapxlz/specter.clj
@@ -22,3 +22,24 @@
     (do (next-fn (ar/resolve-atom (state-kw s)))
         nil)))
 
+(def STATE
+  (path STATE-ATOM ATOM))
+
+(defn partition-in-geo [geometry v]
+  (let [asscending (reduce (fn [v e]
+                             (conj v (+ (or (last v) 0) e)))
+                           [] geometry)
+        parts (partition 2 1 (into [0] asscending))]
+    (into [] (map #(apply (partial subvec (vec v)) %) parts))))
+
+(defn concatv [vs]
+  (reduce (fn [result v]
+            (apply conj result v))
+          [] vs))
+
+(m/defnav geometry-parts
+  [geometry]
+  (select* [this s next-fn]
+    (next-fn (partition-in-geo geometry s)))
+  (transform* [this s next-fn]
+    (concatv (next-fn (partition-in-geo geometry s)))))
