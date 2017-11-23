ee1ebbe2f303d222918861ae1c7171601f7d743c
Albrecht Schmidt <mail@albrechtschmidt.de>
2017-11-24 00:46:48 +0100

    fixed registry test

--- test/pentapxlz/processes/registry_test.clj
+++ test/pentapxlz/processes/registry_test.clj
@@ -11,23 +11,23 @@
 (deftest registry-lifecycle
   (with-redefs [pentapxlz.processes.registry/registry (atom {})]
     (testing "throws errors if not registered"
-      (is (thrown? ExceptionInfo (start ::test)) "Cannot start unregistered process")
-      (is (thrown? ExceptionInfo (stop ::test)) "Cannot stop unregistered process"))
+      (is (thrown? ExceptionInfo (start! ::test)) "Cannot start unregistered process")
+      (is (thrown? ExceptionInfo (stop! ::test)) "Cannot stop unregistered process"))
     (testing "register and starting"
       (register ::test test-process)
       (is (zero? (get-in @registry [::test :state])) "Registering adds process-map to registry")
-      (start ::test)
+      (start! ::test)
       (is (= 1 (get-in @registry [::test :state])) "Starting calls start-fn and replaced process-map by result"))
     (testing "reregister not possible (only with other keyword)"
       (is (thrown? ExceptionInfo (register ::test test-process)) "Cannot reregister under same key")
       (register ::test2 test-process)
       (is (@registry ::test2) "Register under other keyword is possible"))
     (testing "restart not possible by start"
-      (is (thrown? ExceptionInfo (start ::test)) "Cannot start started process"))
+      (is (thrown? ExceptionInfo (start! ::test)) "Cannot start started process"))
     (testing "stopping, starting"
-      (stop ::test)
+      (stop! ::test)
       (is (zero? (get-in @registry [::test :state])) "Stopping calls stop-fn and replaces process-map by result")
-      (start ::test)
+      (start! ::test)
       (is (= 1 (get-in @registry [::test :state])) "Start after stop works as expected"))
     (testing "unregister"
       (unregister ::test)
